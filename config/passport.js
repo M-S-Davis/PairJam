4fc056a3515eafb6f785c25c0008dff77f163199
Mike Davis <Mecro@live.ca>
2022-09-20 22:23:25 -0400

    checkpoint: login auth with google working as intended

--- config/passport.js
+++ config/passport.js
@@ -1,33 +1,38 @@
-const LocalStrategy = require("passport-local").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
 const User = require("../models/User");
 
 module.exports = function (passport) {
   passport.use(
-    new LocalStrategy({ usernameField: "User Name" }, (userName, done) => {
-      User.findOne({ userName: userName }, (err, user) => {
-        if (err) {
-          return done(err);
-        }
-        if (!user) {
-          return done(null, false, { msg: `User name ${userName} not found.` });
-        }
-        if (!user.password) {
-          return done(null, false, {
-            msg: "Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.",
-          });
-        }
-        user.comparePassword(password, (err, isMatch) => {
-          if (err) {
-            return done(err);
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "/auth/google/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        const newUser = {
+          googleId: profile.id,
+          displayName: profile.displayName,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName || "Anon",
+          image: profile.photos[0].value,
+        };
+
+        try {
+          let user = await User.findOne({ googleId: profile.id });
+
+          if (user) {
+            done(null, user);
+          } else {
+            user = await User.create(newUser);
+            done(null, user);
           }
-          if (isMatch) {
-            return done(null, user);
-          }
-          return done(null, false, { msg: "Invalid user name or password." });
-        });
-      });
-    })
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    )
   );
 
   passport.serializeUser((user, done) => {
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -7,7 +8,10 @@ const MongoStore = require("connect-mongo");
 const logger = require("morgan");
 const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
+const authRoutes = require("./routes/auth");
 const dashboardRoutes = require("./routes/dashboard");
+
+const PORT = process.env.PORT || 5000;
 
 // Load config
 require("dotenv").config({ path: "./config/.env" });
@@ -22,16 +26,18 @@ require("./config/passport")(passport);
 app.set("view engine", "ejs");
 
 // Static files
-app.use(express.static(__dirname + "/public"));
-app.use(express.static(__dirname + "/views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "views")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(logger("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(logger("dev"));
+}
 
 // Sessions
 app.use(
   session({
-    secret: "peachy",
+    secret: "secret pear",
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
@@ -44,9 +50,13 @@ app.use(passport.session());
 
 // Setting routes
 app.use("/", mainRoutes);
+app.use("/auth", authRoutes);
 app.use("/dashboard", dashboardRoutes);
 
 // Starting server
-app.listen(process.env.PORT, () => {
-  console.log(`The server is running on port ${process.env.PORT}`);
-});
+app.listen(
+  PORT,
+  console.log(
+    `The server is in ${process.env.NODE_ENV} mode running on port ${PORT}`
+  )
+);
