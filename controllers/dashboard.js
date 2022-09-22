7158a1e2ac2f8a33e11229250e0397717a0cef4c
Mike Davis <Mecro@live.ca>
2022-09-21 23:25:47 -0400

    checkpoint2 user login working as intended with logout

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,7 +1,15 @@
 module.exports = {
-  showDashboard: async (req, res) => {
+  getDashboard: async (req, res) => {
     try {
       res.render("dashboard.ejs", { title: "Dashboard" });
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  getFriends: async (req, res) => {
+    console.log("I made a friend!");
+    try {
+      res.render("friends.ejs", { title: "Friends" });
     } catch (err) {
       console.error(err);
     }
