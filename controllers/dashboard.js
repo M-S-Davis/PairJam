e2f9df320e2d8975e2d6511e4cb0bb7bec674f14
Mike Davis <Mecro@live.ca>
2022-10-06 23:54:56 -0400

    tweaks to styles, fonts added, background images wip

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,9 +1,24 @@
+const Song = require("../models/Song");
+
 module.exports = {
   getDashboard: async (req, res) => {
     try {
+      // const mostRecentSongCreated = await Song.findOne(
+      //   {},
+      //   {},
+      //   { sort: { created_at: 1 } },
+      //   function (err, post) {
+      //     console.log(post);
+      //   }
+      // );
+      const gettingTheNewShit = await Song.findOne().sort({
+        createdAt: "asc",
+        _id: -1,
+      });
       res.render("dashboard.ejs", {
         title: "Dashboard",
         user: req.user,
+        song: gettingTheNewShit,
       });
     } catch (err) {
       console.error(err);
