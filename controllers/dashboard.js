1b728233aafb1274d33792a7445bf5d814f44ae4
Davis <M-S-Davis@users.noreply.github.com>
2022-10-11 19:16:15 -0400

    Styling (#13)
    * members can now be added via bandInfo modal
    
    * $push progress
    
    * progress is slow, switching to frontend
    
    * styling added to views
    
    * deleting members of bands working. note of small issue noticed for later, and issue #1 created
    
    * Reworded to reflect new structure language
    
    * styling improved on Songs page and new templating on band and bandinfo pages
    
    * tweaks to styles, fonts added, background images wip
    
    * various styling improvements
    
    * total bands now displayed on dashboard and added to controller
    
    * landing info moved from dashboard to landing page

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,9 +1,25 @@
+const Song = require("../models/Song");
+const Band = require("../models/Band");
+
 module.exports = {
   getDashboard: async (req, res) => {
     try {
+      const totalBands = await Band.count({
+        createdByGoogleId: req.user.googleId,
+      });
+      const totalSongs = await Song.count({
+        createdByGoogleId: req.user.googleId,
+      });
+      const gettingTheNewShit = await Song.findOne().sort({
+        createdAt: "asc",
+        _id: -1,
+      });
       res.render("dashboard.ejs", {
         title: "Dashboard",
         user: req.user,
+        song: gettingTheNewShit,
+        totalSongs: totalSongs,
+        totalBands: totalBands,
       });
     } catch (err) {
       console.error(err);
